987558a9b54f988d7cf3ce76ea49d608c22ec297
ToufiqAlahi <nipuntan01@gmail.com>
2022-05-25 00:12:23 +0600

    added DELETE method in server side

--- index.js
+++ index.js
@@ -42,7 +42,17 @@ async function run() {
             res.send(result);
         });
 
+        //Delete
+
+        app.delete("/service/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+        });
+
         
+
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
